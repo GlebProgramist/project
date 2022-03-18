6e0d67824f1b7c82f3742b96c57766ce14951bbe
Gleb <glebotron03@gmail.com>
2022-03-18 11:03:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+
+const obj = {
+    name: "Mark",
+    work: "bomj",
+    age: 5,
+    isLox: true
+};
+console.log(obj.isLox);
+
+let arr = ['giga', 2, 'kek', true];
+console.log(arr[3]);
+const result = confirm("are you here");
+const ans = prompt("Ты даун", "Да");
+console.log(ans);
+
+const zopa = "lil";
+console.log(`https://someurl.com/${zopa}/5`);
+
+const daun = 'Vlad';
+console.log("privet"+daun);
