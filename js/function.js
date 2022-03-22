6649504dc1c48fcbcb5075c85671f72fba6a1fda
Gleb <glebotron03@gmail.com>
2022-03-22 12:46:12 +0200

    proga do usovershenstvovaniya

--- js/function.js
+++ js/function.js
@@ -21,3 +21,14 @@ console.log(per);
 
 const a  = 'Mark umniy';
 console.log(a.slice(5,10));
+
+function Learn(lang, callBack){
+    console.log(`ya viuchil: ${lang}`);
+    callBack();
+}
+
+function cl (){
+    console.log('kdkk');
+}
+
+Learn('JS', cl);
